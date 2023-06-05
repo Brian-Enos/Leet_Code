666fd36d7d7b34bbf72518b96d9daae30a7ca476
Brian-Enos <brianenos9198@gmail.com>
2023-06-05 09:10:32 +0300

    Completed the challenge in Python, The output is as desired

---
+++ Fizz-Buzz/Python/Fizz-Buzz.py
@@ -0,0 +1,14 @@
+
+number = int(input("Please enter the number you would like to FizzBuzz To: "))
+
+i = 1
+while i <= number:
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
+    i += 1
